dd2717e32ad83cf18205c5f733dd21c75c87a476
Carlos Congosto <kko-tb_92@hotmail.com>
2015-10-21 16:40:05 +0200

    yaml con snake

--- MainpruebaYAML.java
+++ MainpruebaYAML.java
@@ -1,18 +1,33 @@
 package pruebaYAML;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
 import java.util.Map;
 
+import org.yaml.snakeyaml.Yaml;
+
+import net.sourceforge.yamlbeans.YamlConfig;
+
+/*
+import net.sourceforge.yamlbeans.YamlConfig;
 import net.sourceforge.yamlbeans.YamlException;
 import net.sourceforge.yamlbeans.YamlReader;
+*/
 
-public class Main {
+public class MainpruebaYAML {
 
 	public static void main(String[] args) {
+		
+		/*
+		 * Con yamlbeans
 		YamlReader reader = null;
 		try {
-			reader = new YamlReader(new FileReader("Tema1.yml"));
+			reader = new YamlReader(new FileReader("Tema2.yml"));
 		} catch (FileNotFoundException e) {	
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -25,11 +40,29 @@ public class Main {
 			e.printStackTrace();
 		}
 		//System.out.println(object);
+		if (object == null)
+			System.out.println("No hay objeto");
 		 if (object instanceof Map) {
 			    object=(Map<String,Object>)object;
 			  }
 		Map map = (Map)object;
-		System.out.println(map.get("Resultado"));
+		System.out.println(map.get("Introducción"));*/
+		
+		///Con snakeyaml
+		Yaml yaml = new Yaml();
+		InputStream input = null;
+		try {
+			input = new FileInputStream("Tema1.yml");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		@SuppressWarnings("unchecked")
+		Map<String, String> yamlParsers = (Map<String, String>) yaml.load(input);
+		System.out.println(yamlParsers.get("Lecciones"));
+		
+		
 	}
 
 }
