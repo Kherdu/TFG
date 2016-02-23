42689c07e772ac915139dc27377906f5f58fa27a
Carlos Congosto <kko-tb_92@hotmail.com>
2016-02-23 18:33:31 +0100

    python tema1 rulando

--- TFG/resources/exec/tema1.py
+++ TFG/resources/exec/tema1.py
@@ -1,15 +1,63 @@
 # -*- coding: UTF-8 -*-
 import sys
 
-class Tema1:
-	def pregunta11(code):
-		print(code)
-    	try:
-        	exec(code)
-	        exec("print(x)")
-	        r = "bien"
-    	except Exception:
-	    	print ("se ha roto")
-	    	r="roto"
-    	return r
-    
+def pregunta11(codigo):
+    print("Esto es la pregunta 11")
+    print(codigo)
+    try:
+        exec(codigo)
+        if locals()['i']== 4:
+            print("todo correcto")
+            return True
+        else:
+            print("la variable no contiene el valor correcto")
+            return False
+    except Exception:
+        print("ROTO")
+        return False
+
+def pregunta21(codigo):
+    print("Esto es la pregunta 21")
+    print(codigo)
+    try:
+        exec(codigo)
+        if locals()['cadena']=="Hola mundo":
+            print("Todo correcto")
+            return True
+        else:
+            print("Fallo en el valor")
+            return False
+    except Exception:
+            print("Variable no correcta")
+            return False
+        
+   
+def pregunta22(codigo):
+    print("Esto es la pregunta 22")
+    print(codigo)
+    try:
+        exec(codigo)
+        lista = ["gato", "perro", "burro"]
+        lista.sort
+        print(locals()['lista_animales'].sort)
+        if locals()['lista_animales'].sort==lista:
+            print("Todo corecto")
+            return True;
+        else:
+            print("Fallo de valor")
+            return False;
+    except Exception:
+        print ("Variable no buena")
+        return False
+
+pregunta = sys.argv[1]
+codigo = sys.argv[2]
+
+if pregunta == "pregunta11":
+    pregunta11(codigo)
+elif pregunta == "pregunta21":
+    pregunta21(codigo)
+elif pregunta == "pregunta22":
+    pregunta22(codigo)
+
+
