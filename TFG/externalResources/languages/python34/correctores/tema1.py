f490c6385bbcb42910f1a0ad50da69bef61dd7a2
Enrique Martín <emartinm@fdi.ucm.es>
2016-05-18 17:13:28 +0200

    Corregidas erratas y mejorada retroalimentación correctores

--- TFG/externalResources/languages/python34/correctores/tema1.py
+++ TFG/externalResources/languages/python34/correctores/tema1.py
@@ -6,7 +6,7 @@ import tempfile
 # campo 1: boolean isCorrect: true si está bien, false si está mal
 # campo 2: string typeError: titulo del fallo para poner el mensaje en el label de java
 # campo 3: lista de string Hints: pistas adicionales para mostrar
-def pregunta11(code):
+def pregunta11(code, filename):
     
     try:
         exec(code)
@@ -28,7 +28,7 @@ def pregunta11(code):
         dicc['isCorrect'] = value1
         dicc['typeError'] = value2
         dicc['Hints'] = value3
-        with open(file, 'w') as outfile:
+        with open(filename, 'w') as outfile:
             json.dump(dicc, outfile)
         sys.exit(0);    
     except Exception as e:
@@ -36,25 +36,30 @@ def pregunta11(code):
         sys.exit(1)
         #return False
     
-def pregunta21(code):
+def pregunta21(code, filename):
     
     try:
         exec(code)
-        if locals()['cadena']=="Hola mundo":
-            #print("Todo correcto")
-            sys.exit(0)
-            #return True
+        dicc = {}
+        dicc['typeError'] = ''
+        if "cadena" in locals():
+          dicc['isCorrect'] = (locals()['cadena']=="Hola mundo")
+          dicc['Hints'] = ['El valor de la variable \'cadena\' no es el correcto',' * Valor esperado: Hola mundo' ,' * Valor obtenido: ' + str(locals()['cadena']) ]
         else:
-           # print("Fallo en el valor")
-            sys.exit(1)
-           # return False
-    except Exception:
-            #print("Variable no correcta")
-            sys.exit(-1)
-            #return False
+          dicc['isCorrect'] = False
+          dicc['Hints'] = ['La variable \'cadena\' no ha sido definida']
+       
+        with open(filename, 'w') as outfile:
+            json.dump(dicc, outfile)
+        sys.exit(0)
+    except Exception as e:
+        print(e)
+        sys.exit(-1)
+        #return False
         
    
-def pregunta22(code):
+def pregunta22(code, filename):
+    # TODO arreglar este código, no genera JSON
     try:
         exec(code)
         lista = ["gato", "perro", "burro"]
@@ -71,17 +76,22 @@ def pregunta22(code):
         sys.exit(-1)
        # return False
 
-question = sys.argv[1]
-code = sys.argv[2]
-temp_name = next(tempfile._get_candidate_names())
-default_tmp_dir = tempfile._get_default_tempdir()
-file = default_tmp_dir + '/' + sys.argv[3]
 
-if question == "pregunta11":
-    pregunta11(code)
-elif question == "pregunta21":
-    pregunta21(code)
-elif question == "pregunta22":
-    pregunta22(code)
+def main():
+  question = sys.argv[1]
+  code = sys.argv[2]
+  temp_name = next(tempfile._get_candidate_names())
+  default_tmp_dir = tempfile._get_default_tempdir()
+  filename = default_tmp_dir + '/' + sys.argv[3]
 
+  if question == "pregunta11":
+    pregunta11(code, filename)
+  elif question == "pregunta21":
+    pregunta21(code, filename)
+  elif question == "pregunta22":
+    pregunta22(code, filename)
+    
+    
 
+if __name__ == "__main__":
+  main()    
